da4a88c5b56218924463dfc3e6d19cced9e184c1
ibrahim.kedir <ibrahim.kedir@outlook.com>
2023-11-18 13:35:05 -0500

    update to repository

--- controls.js
+++ controls.js
@@ -1,54 +1,52 @@
-class Controls {
-    // controls of the car 
-    constructor(forward, left, right, reverse) {
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
-// # makes it a private methode 
-        this.#addKeyboardListeners();
-    }
-    
-    // on push of button
-    #addKeyboardListeners() {
-        // the arrow function allows you to refer to the this. above 
-        document.onkeydown = (event) => {
-            switch (event.key) {
-                case "ArrowLeft":
-                    this.left = true;
-                    break;
-                case "ArrowRight":
-                    this.right = true;
-                    break;
-                case "ArrowDown":
-                    this.reverse = true;
-                    break;
-                case "ArrowUp":
-                    this.forward = true;
-                    break;
-            }
-            // view what buttons are being clicked on the console
-            console.table(this);
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
         }
-        
-        // on release of button
-        document.onkeyup = (event) => {
-            switch (event.key) {
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
                 case "ArrowLeft":
-                    this.left = false;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = false;
-                    break;
-                case "ArrowDown":
-                    this.reverse = false;
+                    this.right=true;
                     break;
                 case "ArrowUp":
-                    this.forward = false;
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
             }
-            // view what buttons are being clicked
-            console.table(this);
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
         }
     }
 }
--- main.js
+++ main.js
@@ -1,23 +1,87 @@
-const canvas=document.getElementById("myCanvas");
-canvas.width=200;
+const Canvas=document.getElementById("Canvas");
+Canvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-const ctx = canvas.getContext("2d");
-const road=new Road(canvas.width/2,canvas.width*0.9);
-const car=new Car(road.getLaneCenter(1),100,30,50);
+const objectCtx = Canvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road=new Road(Canvas.width/2,Canvas.width*0.9);
+
+const N=100;
+const junk=generateJunk(N);
+let bestJunk=junk[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<junk.length;i++){
+        junk[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(junk[i].brain,0.1);
+        }
+    }
+}
+
+const traffic=[
+    new Object(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(0),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(2),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(0),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(1),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(1),-700,30,50,"DUMMY",2,getRandomColor()),
+    new Object(road.getLaneCenter(2),-700,30,50,"DUMMY",2,getRandomColor()),
+];
 
 animate();
 
-function animate(){
-    car.update(road.borders);
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestJunk.brain));
+}
 
-    canvas.height=window.innerHeight;
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
 
-    ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7);
+function generateJunk(N){
+    const junk=[];
+    for(let i=1;i<=N;i++){
+        junk.push(new Object(road.getLaneCenter(1),100,30,50,"AI"));
+    }
+    return junk;
+}
 
-    road.draw(ctx);
-    car.draw(ctx);
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    for(let i=0;i<junk.length;i++){
+        junk[i].update(road.borders,traffic);
+    }
+    bestJunk=junk.find(
+        c=>c.y==Math.min(
+            ...junk.map(c=>c.y)
+        ));
 
-    ctx.restore();
+    Canvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+    objectCtx.save();
+    objectCtx.translate(0,-bestJunk.y+Canvas.height*0.7);
+
+    road.draw(objectCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(objectCtx);
+    }
+    objectCtx.globalAlpha=0.2;
+    for(let i=0;i<junk.length;i++){
+        junk[i].draw(objectCtx);
+    }
+    objectCtx.globalAlpha=1;
+    bestJunk.draw(objectCtx,true);
+
+    objectCtx.restore();
+
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,bestJunk.brain);
     requestAnimationFrame(animate);
 }
