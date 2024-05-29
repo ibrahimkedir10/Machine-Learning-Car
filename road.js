0e3a1d8becaf2ba29912a64300a27c87e3b27e61
ibrahim.kedir <ibrahim.kedir@outlook.com>
2024-05-29 11:11:16 -0400

    Update road.js

--- road.js
+++ road.js
@@ -38,7 +38,7 @@ class Road{
                 i/this.laneCount
             );
             
-            ctx.setLineDash([20,20]);
+            ctx.setLineDash([]);
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
